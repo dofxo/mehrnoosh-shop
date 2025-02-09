f8556b69205e3e69c71df4138e37674eac789e50
Majid Kargar <majidkargar01@gmail.com>
2025-02-09 08:49:32 +0330

    feat: add container in tailwind config spacing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      spacing: {
+        container: "1300px",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
