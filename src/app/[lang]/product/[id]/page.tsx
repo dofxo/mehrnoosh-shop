456753900789107802cb1a710a3b46ecff6d13cb
dofxo <kargat504@gmail.com>
2025-02-09 20:15:49 +0330

    feat: fetch product data

---
+++ src/app/[lang]/product/[id]/page.tsx
@@ -0,0 +1,14 @@
+export default async function Product({ params }: { params: { id: string } }) {
+  // extract id from param and call for product data
+  const productId = await params.id;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/products/${productId}`,
+  );
+
+  const product = await response.json();
+
+  // extract first item from the response array
+  const productData = product[0];
+
+  return <div></div>;
+}
