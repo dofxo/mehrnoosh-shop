75ee1c6a9e96e8b6a2a94ac94adfcdc36153b48a
Majid Kargar <majidkargar01@gmail.com>
2025-02-21 16:51:50 +0330

    feat: add a currentLanguage prop as well

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -1,22 +1,28 @@
-"use client";
-
-import { initializeLanguage } from "../lib/features/language/languageSlice";
-import { makeStore, AppStore } from "../lib/store";
-import { useRef } from "react";
-import { Provider } from "react-redux";
+'use client'
+import { useRef } from 'react'
+import { Provider } from 'react-redux'
+import { makeStore, AppStore } from '../lib/store'
+import { initializeLanguage } from '../lib/features/language/languageSlice'
 
 export default function StoreProvider({
   language,
+  currentLanguage,
   children,
 }: {
-  language: string;
-  children: React.ReactNode;
+  language: Record<string, any>,
+  currentLanguage: string,
+  children: React.ReactNode
 }) {
-  const storeRef = useRef<AppStore | null>(null);
+  const storeRef = useRef<AppStore | null>(null)
   if (!storeRef.current) {
-    storeRef.current = makeStore();
-    storeRef.current.dispatch(initializeLanguage(language));
+    storeRef.current = makeStore()
+    storeRef.current.dispatch(
+      initializeLanguage({
+        languageData: language,
+        currentLanguage,
+      })
+    )
   }
 
-  return <Provider store={storeRef.current}>{children}</Provider>;
+  return <Provider store={storeRef.current}>{children}</Provider>
 }
