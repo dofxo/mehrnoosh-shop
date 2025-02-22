a59b194538c33b4615ff4bad40d5e2b0c00e5d7e
Majid Kargar <majidkargar01@gmail.com>
2025-02-22 16:53:43 +0330

    feat: add some component using both shadcn and radix

---
+++ src/components/ui/direction.tsx
@@ -0,0 +1,18 @@
+import { useAppSelector } from "@/lib/hooks";
+import { Direction } from "radix-ui";
+
+export default function DirectionWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const currentLanguage = useAppSelector(
+    (state: any) => state.language.currentLanguage,
+  );
+
+  return (
+    <Direction.Provider dir={currentLanguage === "fa" ? "rtl" : "ltr"}>
+      {children}
+    </Direction.Provider>
+  );
+}
