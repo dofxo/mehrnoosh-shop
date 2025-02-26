890b7af191b9df813bf5298acd9b4affa6a71b13
Majid Kargar <majidkargar01@gmail.com>
2025-02-26 13:53:30 +0330

    feat: add a mobile drawer using the shadcn sheet component
    It still needs work

---
+++ src/components/ui/header/mobile/MobileDrawer.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Logo from "../../../../../public/images/text-logo.png";
+import DirectionWrapper from "@/components/ui/direction";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import Image from "next/image";
+
+export default function MobileDrawer() {
+  return (
+    <DirectionWrapper>
+      <Sheet>
+        <SheetTrigger>
+          <div className="cursor-pointer rounded-primary bg-white p-4 transition hover:bg-primary hover:text-white">
+            <MenuOutlinedIcon />
+          </div>
+        </SheetTrigger>
+        <SheetContent>
+          <div className="mt-8 flex items-center justify-center">
+            <Image src={Logo} width={250} height={63} alt="logo" />
+          </div>
+        </SheetContent>
+      </Sheet>
+    </DirectionWrapper>
+  );
+}
--- src/components/ui/header/mobile/MobileHeader.tsx
+++ src/components/ui/header/mobile/MobileHeader.tsx
@@ -1,16 +1,15 @@
 "use client";
 
-import Logo from "../../../../../public/images/logo.png";
+import MobileDrawer from "@/components/ui/header/mobile/MobileDrawer";
 import Search from "@/components/ui/header/search/Search";
-import Image from "next/image";
 
 export default function MobileHeader() {
   return (
-    <section className="lg:hidden block w-full bg-secondary">
+    <section className="block w-full bg-secondary lg:hidden">
       <div className="container">
         <div className="flex flex-row items-center justify-between">
-          <div className="w-[20%] max-w-[200px] p-[10px]">
-            <Image src={Logo} width={63} height={63} alt="logo" />
+          <div className="flex w-[20%] max-w-[200px] flex-row items-center justify-between gap-1 p-[10px]">
+            <MobileDrawer />
           </div>
           <Search />
         </div>
