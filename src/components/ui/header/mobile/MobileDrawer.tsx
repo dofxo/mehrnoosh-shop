16c1a7109d58bfb8dda54dc2bf6200d4955d7d0b
Majid Kargar <majidkargar01@gmail.com>
2025-03-08 17:54:22 +0330

    feat: add a feature to change the drawer side based on language

--- src/components/ui/header/mobile/MobileDrawer.tsx
+++ src/components/ui/header/mobile/MobileDrawer.tsx
@@ -4,10 +4,15 @@ import Logo from "../../../../../public/images/text-logo.png";
 import DirectionWrapper from "@/components/ui/direction";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import SidebarMenuItems from "@/components/ui/sidebar/sidebar";
+import { useAppSelector } from "@/lib/hooks";
 import { Menu } from "lucide-react";
 import Image from "next/image";
 
 export default function MobileDrawer() {
+  const currentLanguage = useAppSelector(
+    (state) => state.language.currentLanguage,
+  );
+
   return (
     <DirectionWrapper>
       <Sheet>
@@ -16,7 +21,10 @@ export default function MobileDrawer() {
             <Menu />
           </div>
         </SheetTrigger>
-        <SheetContent className="flex flex-col">
+        <SheetContent
+          className="flex flex-col"
+          side={currentLanguage === "en" ? "left" : "right"}
+        >
           <div className="mt-8 flex items-center justify-center !overflow-hidden">
             <Image src={Logo} width={250} height={63} alt="logo" />
           </div>
