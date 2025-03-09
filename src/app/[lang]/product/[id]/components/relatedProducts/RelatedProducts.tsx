be7021d7d6fc7c1cd7c4b3317619054432989001
dofxo <kargat504@gmail.com>
2025-03-09 19:23:26 +0330

    feat: add related products title icon and styles

--- src/app/[lang]/product/[id]/components/relatedProducts/RelatedProducts.tsx
+++ src/app/[lang]/product/[id]/components/relatedProducts/RelatedProducts.tsx
@@ -1,15 +1,24 @@
 "use client";
 
-import { useAppSelector } from "@/lib/hooks";
 import { IProduct } from "../../Product";
+import { useAppSelector } from "@/lib/hooks";
+import { Store } from "lucide-react";
 
 const RelatedProducts = () => {
-  const { languageData } =
-    useAppSelector((state) => state.language);
+  const { languageData } = useAppSelector((state) => state.language);
 
-  return <section>
-    <h3>{languageData.productSingle.related_products}</h3>
-  </section>;
+  return (
+    <section>
+      <div className="flex items-center gap-2">
+        <div className="flex h-[40px] w-[40px] items-center justify-center rounded-[50%] bg-primary text-[18px] text-white">
+          <Store size={18} />
+        </div>
+        <h3 className="text-[20px] font-bold">
+          {languageData.productSingle.related_products}
+        </h3>
+      </div>
+    </section>
+  );
 };
 
 export default RelatedProducts;
