8354e516b15e0c3c80b7af702b4af51dac8500eb
Majid Kargar <majidkargar01@gmail.com>
2025-03-12 13:03:29 +0330

    refactor: remove extra classes from tigger since they were added to variant

--- src/components/ui/mega-menu/mega-menu-trigger/MegaMenuTrigger.tsx
+++ src/components/ui/mega-menu/mega-menu-trigger/MegaMenuTrigger.tsx
@@ -3,13 +3,9 @@ import { Button } from "@/components/ui/button";
 
 export default function MegaMenuTrigger({ icon, text }: MegaMenuTriggerProps) {
   return (
-    <Button
-      variant="secondary"
-      className="text-[16px] hover:bg-primary hover:text-white"
-      size="lg"
-    >
-      {icon}
-      {text}
+    <Button variant="megaMenu" size="xl">
+      <div>{icon}</div>
+      <span> {text}</span>
     </Button>
   );
 }
