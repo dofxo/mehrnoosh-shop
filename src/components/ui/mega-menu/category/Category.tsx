a668575772ae5546d654247b204f1d0781778bfa
Majid Kargar <majidkargar01@gmail.com>
2025-03-14 17:08:22 +0330

    feat: change style of menu content bg

--- src/components/ui/mega-menu/category/Category.tsx
+++ src/components/ui/mega-menu/category/Category.tsx
@@ -1,4 +1,4 @@
-import type { MegaMenuCategoryProps } from "./Category.d"
+import type { MegaMenuCategoryProps } from "./Category.d";
 import MegaMenuTrigger from "@/components/ui/mega-menu/mega-menu-trigger/MegaMenuTrigger";
 import { MenuContent, MenuRoot, MenuTrigger } from "@/components/ui/menu";
 import { List } from "lucide-react";
@@ -7,12 +7,9 @@ export default function MegaMenuCategory({ language }: MegaMenuCategoryProps) {
   return (
     <MenuRoot>
       <MenuTrigger asChild>
-        <MegaMenuTrigger
-          icon={<List />}
-          text={language["megaMenu"].category}
-        />
+        <MegaMenuTrigger icon={<List />} text={language["megaMenu"].category} />
       </MenuTrigger>
-      <MenuContent>
+      <MenuContent className="w-[100vw] max-w-container rounded-primary bg-secondary p-[30px] text-black shadow-none">
         <span>some content here!</span>
       </MenuContent>
     </MenuRoot>
