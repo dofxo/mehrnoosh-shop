7e5ea4c5ade917e21f8d2feb79b102fb36c2e159
Majid Kargar <majidkargar01@gmail.com>
2025-03-14 16:11:37 +0330

    refactor: change the mega menu trigger to accept possible props from its parent

--- src/components/ui/mega-menu/mega-menu-trigger/MegaMenuTrigger.tsx
+++ src/components/ui/mega-menu/mega-menu-trigger/MegaMenuTrigger.tsx
@@ -1,11 +1,15 @@
 import type { MegaMenuTriggerProps } from "./MegaMenuTrigger.d";
 import { Button } from "@/components/ui/button";
 
-export default function MegaMenuTrigger({ icon, text }: MegaMenuTriggerProps) {
+export default function MegaMenuTrigger({
+  icon,
+  text,
+  ...props
+}: MegaMenuTriggerProps) {
   return (
-    <Button variant="megaMenu" size="xl">
+    <Button variant="megaMenu" size="xl" {...props}>
       <div className="h-6 w-6">{icon}</div>
-      <span className="-mb-1">{text}</span>
+      <span className="">{text}</span>
     </Button>
   );
 }
