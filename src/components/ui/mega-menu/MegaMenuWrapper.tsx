a2f8904608cb84c0b9231af7292494d5cdf1d496
Majid Kargar <majidkargar01@gmail.com>
2025-03-16 14:00:40 +0330

    feat: Add Shopping basket to the MegaMenuWrapper

--- src/components/ui/mega-menu/MegaMenuWrapper.tsx
+++ src/components/ui/mega-menu/MegaMenuWrapper.tsx
@@ -7,6 +7,7 @@ import MegaMenuHouseEquipments from "@/components/ui/mega-menu/house-equipments/
 import MegaMenuLaptopAndComputer from "@/components/ui/mega-menu/laptop-and-computer/LaptopAndComputer";
 import ShoppingBasketSheet from "@/components/ui/mega-menu/shopping-basket/ShoppingBasket";
 import { useAppSelector } from "@/lib/hooks";
+import { ShoppingBasket as ShoppingBasketIcon } from "lucide-react";
 
 export default function MegaMenuWrapper() {
   const language = useAppSelector((state) => state.language.languageData);
@@ -14,7 +15,7 @@ export default function MegaMenuWrapper() {
   return (
     <section className="hidden bg-white p-[15px] pt-[20px] lg:block">
       <div className="container flex flex-row items-center justify-between">
-        <div className="flex gap-3">
+        <div className="flex gap-1">
           <MegaMenuCategory language={language} />
           <MegaMenuDigitalEquipments language={language} />
           <MegaMenuHouseEquipments language={language} />
@@ -22,9 +23,16 @@ export default function MegaMenuWrapper() {
           <MegaMenuCascadingMenu language={language} />
         </div>
         <div className="flex w-[20%] max-w-[200px] flex-row items-center justify-between gap-1 p-[10px]">
-          <ShoppingBasketSheet />
+          <ShoppingBasketSheet>
+            <div className="group flex cursor-pointer flex-row items-center justify-center gap-2 rounded-primary bg-primary p-4 text-white transition delay-75 hover:bg-primary-100 ltr:flex-row-reverse rtl:flex-row">
+              {/* TODO: make this dynamic  */}
+              <div className="flex items-center justify-center rounded-[100%] bg-primary-700 px-2.5 pt-1 font-bold transition group-hover:bg-primary">
+                0
+              </div>
+              <ShoppingBasketIcon className="transition group-hover:text-primary" />
+            </div>
+          </ShoppingBasketSheet>
         </div>
-
       </div>
     </section>
   );
