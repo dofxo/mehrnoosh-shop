8b4339be9e87c0dacc76b7d54602d1b5af0a42d1
Majid Kargar <majidkargar01@gmail.com>
2025-03-16 14:00:13 +0330

    refactor: change the Shopping basket component to accept the trigger as child

---
+++ src/components/ui/mega-menu/shopping-basket/ShoppingBasket.d.ts
@@ -0,0 +1,3 @@
+export type ShoppingBasketProps = {
+  children: React.ReactNode;
+};
--- src/components/ui/mega-menu/shopping-basket/ShoppingBasket.tsx
+++ src/components/ui/mega-menu/shopping-basket/ShoppingBasket.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import type { ShoppingBasketProps } from "./ShoppingBasket.d";
 import DirectionWrapper from "@/components/ui/direction";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { useAppSelector } from "@/lib/hooks";
-import { ShoppingBasket as ShoppingBasketIcon, Frown } from "lucide-react";
+import { Frown } from "lucide-react";
+import { ChevronLeft } from "lucide-react";
 
-export default function ShoppingBasket() {
+export default function ShoppingBasket({ children }: ShoppingBasketProps) {
   const { currentLanguage, languageData } = useAppSelector(
     (state) => state.language,
   );
@@ -14,20 +15,12 @@ export default function ShoppingBasket() {
   return (
     <DirectionWrapper>
       <Sheet>
-        <SheetTrigger>
-          <div className="group flex cursor-pointer flex-row items-center justify-center gap-2 rounded-primary bg-primary p-4 text-white transition delay-75 hover:bg-primary-100 ltr:flex-row-reverse rtl:flex-row">
-            {/* TODO: make this dynamic  */}
-            <div className="flex items-center justify-center rounded-[100%] bg-primary-700 px-2.5 pt-1 font-bold transition group-hover:bg-primary">
-              0
-            </div>
-            <ShoppingBasketIcon className="transition group-hover:text-primary" />
-          </div>
-        </SheetTrigger>
+        <SheetTrigger>{children}</SheetTrigger>
         <SheetContent
           className="flex flex-col"
           side={currentLanguage === "en" ? "right" : "left"}
         >
-          <div className="relative">
+          <div className="z-1 relative">
             <div className="flex flex-col items-center justify-center gap-4 rounded-primary bg-secondary p-[20px]">
               <div className="rounded-[100%] bg-primary-100 p-6">
                 <Frown
@@ -38,9 +31,15 @@ export default function ShoppingBasket() {
               </div>
               <p>{languageData.shoppingBasket.empty_basket}</p>
             </div>
-            <div className="fixed bottom-2 flex items-center justify-between rounded-primary bg-secondary p-[15px] text-primary">
-              <span>{languageData.shoppingBasket.go_to_shop}</span>
-              <div></div>
+          </div>
+
+          {/* bottom nav */}
+          <div className="group absolute bottom-4 left-2.5 right-2.5 flex cursor-pointer items-center justify-between rounded-primary bg-secondary p-[15px] text-primary">
+            <span className="font-bold">
+              {languageData.shoppingBasket.go_to_shop}
+            </span>
+            <div className="cursor-pointer rounded-[100%] bg-white p-2 transition group-hover:bg-primary group-hover:text-white">
+              <ChevronLeft size={16} />
             </div>
           </div>
         </SheetContent>
