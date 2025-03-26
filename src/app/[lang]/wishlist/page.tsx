d2a8427bc187c9b5214d22d8a3f72d5f0cf1b786
Mohammad Kargar <kargat504@gmail.com>
2025-03-26 13:22:51 +0330

    feat(wishlist): add products list wrapper and styles

--- src/app/[lang]/wishlist/page.tsx
+++ src/app/[lang]/wishlist/page.tsx
@@ -1,17 +1,23 @@
 "use client";
 
 import { useAppSelector } from "@/lib/hooks";
+import { Heart } from "lucide-react";
 
 const WhishList = () => {
   const language = useAppSelector((state) => state.language.languageData);
 
   return (
     <section className="container !mt-[20px] rounded-[25px] bg-white p-[30px]">
-      <h1 className="text-2xl font-bold text-text-primary">
-        {language.whishlist.title}
+      <h1 className="flex items-center gap-3 text-2xl font-bold text-text-primary">
+        <div className="flex h-[40px] w-[40px] items-center justify-center rounded-[50%] bg-[#ff4191] shadow-[0px_2px_15px_#FF419170]">
+          <Heart color="white" />
+        </div>
+        <span> {language.whishlist.title}</span>
       </h1>
 
-      <div className="list-of-products"></div>
+      <div className="list-of-products mt-[30px] rounded-[20px] bg-background p-[20px]">
+        <div className="rounded-[15px] bg-white p-[20px]"></div>
+      </div>
     </section>
   );
 };
