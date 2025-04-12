a7b953cae0e9ba0d3be124a50086bc4460144445
Majid Kargar <majidkargar01@gmail.com>
2025-04-12 14:07:57 +0330

    feat: add some basic styles and images to each product

--- src/components/ui/mega-menu/category/Category.tsx
+++ src/components/ui/mega-menu/category/Category.tsx
@@ -2,9 +2,11 @@ import type { MegaMenuCategoryProps } from "./Category.d";
 import MegaMenuTrigger from "@/components/ui/mega-menu/mega-menu-trigger/MegaMenuTrigger";
 import { MenuContent, MenuRoot, MenuTrigger } from "@/components/ui/menu";
 import { Smartphone, LaptopMinimal } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+import { AiOutlineLoading } from "react-icons/ai";
 import { GiLipstick, GiVacuumCleaner, GiSmallFire } from "react-icons/gi";
 
-// TODO: change it to a component that renders each category
 export default function MegaMenuCategory({
   currentLanguage,
   category,
@@ -17,8 +19,6 @@ export default function MegaMenuCategory({
   const currentCategoryProducts = products.productsData.filter(
     (product: any) => product.category.en[0] === category.en[0],
   );
-
-  console.log(currentCategoryProducts);
 
   const categoryIconObject: Record<string, React.ReactElement> = {
     "laptop and computer": <LaptopMinimal />,
@@ -36,11 +36,41 @@ export default function MegaMenuCategory({
       <MenuTrigger asChild>
         <MegaMenuTrigger icon={categoryIcon} text={categoryText} />
       </MenuTrigger>
-      <MenuContent dir={currentLanguage === "fa" ? "rtl" : "ltr"} className="mt-10 w-[100vw] max-w-container rounded-primary bg-white p-[30px] text-black shadow-none">
+      <MenuContent
+        dir={currentLanguage === "fa" ? "rtl" : "ltr"}
+        className="hidden lg:flex mt-10 w-[100vw] max-w-container flex-wrap justify-start gap-2 rounded-primary bg-white p-[30px] text-black shadow-none"
+      >
         {/* TODO: change prop type later */}
-        {currentCategoryProducts.map((item: any) => {
+        {currentCategoryProducts.map((item: any, idx: number) => {
           const itemName = item.name[currentLanguage];
-          return <div>{itemName}</div>;
+          const [isLoading, setIsLoading] = useState(true);
+
+          return (
+            <div
+              key={idx}
+              className="flex max-w-[content] flex-col items-center gap-2 rounded-primary border-[10px] border-secondary p-3"
+            >
+              <div className="h-[100px] w-[100px]">
+                {isLoading && (
+                  <div className="rounded absolute inset-0 flex animate-pulse items-center justify-center bg-gray-100">
+                    {/* TODO: change this */}
+                    <AiOutlineLoading />
+                  </div>
+                )}
+                <Image
+                  width={100}
+                  height={50}
+                  alt={itemName}
+                  src={item.images[0]}
+                  onLoad={() => setIsLoading(false)}
+                  className={`transition-opacity duration-300 ${isLoading ? "opacity-0" : "opacity-100"}`}
+                />
+              </div>
+              <span className="min-w-[150px] max-w-[250px] font-bold ltr:text-left rtl:text-right">
+                {itemName}
+              </span>
+            </div>
+          );
         })}
       </MenuContent>
     </MenuRoot>
