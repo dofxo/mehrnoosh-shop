08e74aa8b1a7c7c48809e372e23c39ea5eea1692
Majid Kargar <majidkargar01@gmail.com>
2025-05-18 13:07:16 +0330

    feat: implemented the filter change options feature

--- src/app/[lang]/shop/components/sort/sort.tsx
+++ src/app/[lang]/shop/components/sort/sort.tsx
@@ -1,20 +1,53 @@
 "use client";
 
 import type { SortProps } from "./sort.d";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { filterOptions } from "@/configs/filterOptions";
 import { useAppSelector } from "@/lib/hooks";
+import { cn } from "@/lib/utils";
+import { useState, useEffect } from "react";
 
 export default function Sort({ productsAmount }: SortProps) {
   const language = useAppSelector((state) => state.language.languageData);
+  const currentLanguage = useAppSelector(
+    (state) => state.language.currentLanguage,
+  ) as "en" | "fa";
+
+  const [currentFilter, setCurrentFilter] = useState("default");
+
+  const handleCurrentFilterChange = (value: string) => {
+    setCurrentFilter(value);
+  };
+
+  // TODO: change this later
+  useEffect(() => {
+    console.log("Filter changed to:", currentFilter);
+  }, [currentFilter]);
 
   return (
     <>
       <h2 className="mb-[20px] text-[25px] font-bold">{language.shop.title}</h2>
-      <div className="flex flex-row gap-2">
-        <div className="rounded-primary bg-white px-[15px] py-[8px]">
-          {/* TODO: change 1-9 to show the related number in the products list */}
+
+      <div className="flex flex-row flex-wrap items-center gap-2">
+        <div className="rounded-primary bg-white px-[16px] py-[8px] text-sm">
           {language.shop.show} 1-9 {language.shop.from} {productsAmount}{" "}
           {language.shop.result}
         </div>
+
+        <RadioGroup
+          value={currentFilter}
+          onValueChange={handleCurrentFilterChange}
+          className={cn(
+            "flex flex-wrap gap-2",
+            currentLanguage === "fa" ? "flex-row-reverse" : "flex-row",
+          )}
+        >
+          {filterOptions.map((option) => (
+            <RadioGroupItem key={option.value} value={option.value}>
+              {option.label[currentLanguage]}
+            </RadioGroupItem>
+          ))}
+        </RadioGroup>
       </div>
     </>
   );
---
+++ src/configs/filterOptions.ts
@@ -0,0 +1,44 @@
+export const filterOptions = [
+  {
+    value: "default",
+    label: {
+      en: "Default",
+      fa: "مرتب سازی پیش‌فرض",
+    },
+  },
+  {
+    value: "most-sold",
+    label: {
+      en: "Most Sold",
+      fa: "پرفروش‌ترین",
+    },
+  },
+  {
+    value: "highest-score",
+    label: {
+      en: "Highest Score",
+      fa: "بالاترین امتیاز",
+    },
+  },
+  {
+    value: "newest",
+    label: {
+      en: "Newest",
+      fa: "جدیدترین",
+    },
+  },
+  {
+    value: "cheapest",
+    label: {
+      en: "Cheapest",
+      fa: "ارزان‌ترین",
+    },
+  },
+  {
+    value: "most-expensive",
+    label: {
+      en: "Most Expensive",
+      fa: "گران‌ترین",
+    },
+  },
+];
