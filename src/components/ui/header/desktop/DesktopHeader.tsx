7661ba3821f4a97c86ad3d2b2ef9f48d4e292eda
Majid Kargar <majidkargar01@gmail.com>
2025-05-31 18:34:35 +0330

    feat: add a link to the logo in header

--- src/components/ui/header/desktop/DesktopHeader.tsx
+++ src/components/ui/header/desktop/DesktopHeader.tsx
@@ -4,15 +4,16 @@ import Logo from "../../../../../public/images/text-logo.png";
 import DesktopHeaderEnd from "./DesktopHeaderEnd";
 import Search from "@/components/ui/header/search/Search";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function DesktopHeader() {
   return (
-    <section className="hidden lg:block w-full bg-secondary p-[15px]">
+    <section className="hidden w-full bg-secondary p-[15px] lg:block">
       <div className="container">
         <div className="flex flex-row items-center justify-center">
-          <div className="w-[20%] max-w-[200px] p-[10px]">
+          <Link href="/" className="w-[20%] max-w-[200px] p-[10px]">
             <Image src={Logo} width={175} height={63} alt="logo" />
-          </div>
+          </Link>
           <Search />
           <DesktopHeaderEnd />
         </div>
